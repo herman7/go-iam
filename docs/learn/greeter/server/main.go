64fc655433c3c77870eff8dc2b92bb8efdfd884b
herman365 <hermango365@gmail.com>
2024-12-25 17:37:06 +0800

    feat:grpc

---
+++ docs/learn/greeter/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/herman7/go-iam/docs/learn/greeter/helloworld"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("failed to server: %v", err)
+	}
+}
